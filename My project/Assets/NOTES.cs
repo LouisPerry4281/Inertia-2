675e36d26c177bcd68852c4fe3b89fe04c17359a
LouisPerry4281 <louisperry4281@gmail.com>
2026-05-03 14:57:22 +0100

    Scene Setups

---
+++ My project/Assets/NOTES.cs
@@ -0,0 +1,3 @@
+// Player Movement
+// Camera Setup
+// 
---
+++ My project/Assets/Scripts/Characters/Player/PlayerLocomotion.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerLocomotion : CharacterLocomotion
+{
+    private Vector2 inputVector;
+    
+    void Awake()
+    {
+        
+    }
+}
